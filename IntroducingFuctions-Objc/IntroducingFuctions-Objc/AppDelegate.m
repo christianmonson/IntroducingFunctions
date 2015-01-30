fa8a3fef45daa6e75950d37e39f19e05639e6715
christianmonson <christianrmonson@gmail.com>
2015-01-29 22:42:15 -0700

    completes project

--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -17,13 +17,38 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Insert code here...
-    
+    NSString *myName;
+    myName = @"Christian";
+    [self countDownTillIntroduction:4];
+    [self introductions:myName];
     
     return YES;
 }
 
-// Define new functions here...
+- (void)introductions:(NSString *)name {
+    
+    NSString *introduction = [NSString stringWithFormat:@"my name is %@", name];
+    NSString *japaneseIntroduction = [name stringByAppendingString:@" to moushimasu"];
+    NSLog(@"%@", introduction);
+    NSLog(@"%@", japaneseIntroduction);
+    
+}
+
+- (void)countDownTillIntroduction:(int)numberOfDays; {
+
+    if (numberOfDays == 0){
+    
+    NSLog(@"The time has come.");
+    
+    } else {
+        
+        NSLog(@"%d days until introductions", numberOfDays);
+    
+        int oneLessDay;
+        oneLessDay = numberOfDays - 1;
+        [self countDownTillIntroduction:oneLessDay];
+    }
+}
 
 
 
